821d41b11e262bd82e7e7772012fc0a699299fcd
X-ZERO <x-zero@zeromacbook-pro.lan>
2017-03-01 09:32:12 +0800

    Initial Commit

---
+++ FirstTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FirstTest
+//
+//  Created by X-ZERO on 2017/3/1.
+//  Copyright © 2017年 X-ZERO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FirstTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FirstTest
+//
+//  Created by X-ZERO on 2017/3/1.
+//  Copyright © 2017年 X-ZERO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
